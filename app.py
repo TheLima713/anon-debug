cb19915014179b36f90235ec55a52ee3fa41ed0f
emyguidi07 <emy.guidi.cavalcante2020@gmail.com>
2025-11-26 20:53:47 -0300

    updated count access in app.py

--- app.py
+++ app.py
@@ -1,46 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,session
 from anonimizador import anonimizar_texto
 
 import requests
 import os
 import json
-bin_key = os.environ['BIN_KEY']
-bin_url = 'https://api.jsonbin.io/v3/b/6925a2e1d0ea881f40ff3959'
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "qualquercoisa"
+app.permanent_session_lifetime = timedelta(days=365)
+
+contador_total = 0
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-    #get contador
-    response = requests.get(
-        bin_url,
-        headers = {
-            'X-Master-Key':bin_key
-        }
-    )
-    data = response.json()
-    
-    #update contador
-    data['record']['contador_acessos'] += 1
-    response = requests.put(
-        bin_url,
-        headers = {
-            'Content-Type': 'application/json',
-            'X-Master-Key':bin_key
-        },
-        data = json.dumps(data['record'])
-    )
-    contador = data['record']['contador_acessos']
+    global contador_total
+    if not session.get("contou"):
+        contador_total += 1
+        session["contou"] = True
+
 
     if request.method == "POST":
         lei = request.form.get("lei")
         texto = request.form.get("prompt", "")
         if not lei:
-            return render_template("index.html", original=None, erro="Selecione uma legislação antes de continuar.", contador=contador)
+            return render_template("index.html", original=None, erro="Selecione uma legislação antes de continuar.", contador=contador_total)
         texto_anon = anonimizar_texto(texto, lei)
-        return render_template("index.html", original=texto, anonimizado=texto_anon, contador=contador)
-    return render_template("index.html", original=None, contador=contador)
+        return render_template("index.html", original=texto, anonimizado=texto_anon, contador=contador_total)
+    return render_template("index.html", original=None, contador=contador_total)
 
 if __name__ == "__main__":
     app.run(debug=True)
